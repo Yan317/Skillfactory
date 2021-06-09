3faf30a3f7e995d0f6d0fc5487b25911ad7b8692
Yan317 <igor_kuz@list.ru>
2021-06-09 12:20:06 +0300

    Tic-tac-toe. Уровень рабочего шаблона.

--- Tic-tac-toe/Tic-tac-toe.py
+++ Tic-tac-toe/Tic-tac-toe.py
@@ -1,8 +1,52 @@
+import numpy as np
+
+
 def print_play_field(play_field_list):
     '''Процедура отрисовывает игровое поле в консоле.'''
-    print('   0  1  2')
-    for i in range(0, 3):
+    print('    0  1  2')  # нумирация столбцов
+    for i in range(0, 3):  # цикл отрисовки строк
         print(i, ' ', '  '.join(play_field_list[i]))
+
+
+def move_user(play_field_list):
+    '''Функция обработкит хода пользователся.
+       На вход функции подается игровое поле до хода пользователся.
+       На выходе функции игровое поле после с учётом хода пользователся.'''
+    while True:
+        print_play_field(play_field_list)  # выводим текущую ситуацию на игровом поле
+        move_user_str = input('Ваш ход. (строка, столбец): ')
+        move_user_list = list(map(int, move_user_str.split(',')))  # преобразуем в список координат
+        if move_user_list[0] > 2 or move_user_list[1] > 2:
+            print('На поле нет такой ячейки. Попробуйте ещё раз.')
+            continue
+        if play_field_list[move_user_list[0]][move_user_list[1]] == '-':
+            play_field_list[move_user_list[0]][move_user_list[1]] = 'x'
+            break
+        else:
+            print('Указанная вами ячейка уже занята. Попробуйте ещё раз.')
+    return play_field_list
+
+
+def move_computer(play_field_list):
+    '''Функция обработки хода компьютером.
+       На вход функции подается игровое поле до хода пользователся.
+       На выходе функции игровое поле после с учётом хода пользователся.
+       Компьютер делает свой ход на угад - в любую свободную ячейку, "не думая".'''
+
+    # для начала выясним количество возможных ходов (оставшихся пустых ячеек)
+    play_field_list_line = sum(play_field_list, [])  # приведём список списков "в одну линию"
+    count_moves_left = len(list(filter(lambda m: m == '-', play_field_list_line)))
+
+    # выбор хода и проставление "нолика"
+    move_select = np.random.randint(1, count_moves_left)
+    for row in range(len(play_field_list)):
+        for column in range(len(play_field_list[row])):
+            if play_field_list[row][column] == '-':
+                move_select -= 1
+                if not move_select:
+                    play_field_list[row][column] = 'o'
+
+    return play_field_list
 
 
 def one_game(score_user_comp):
@@ -12,10 +56,15 @@ def one_game(score_user_comp):
 
     play_field_list = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]  # задаем модель, пока пустого, игрового поля
     print('Ходите первым.')
-    print_play_field(play_field_list)
-    move_user_str = input('Ваш ход. (строка, столбец): ')
-
-
+    move_user_bool = True  # переменная определяющая чей ход
+    while '-' in sum(play_field_list, []):  # проверка наличия "пустых" ячеек для хода
+        if move_user_bool:
+            play_field_list = move_user(play_field_list)
+            move_user_bool = False
+        else:
+            play_field_list = move_computer(play_field_list)
+            move_user_bool = True
+        # проверяем не закончилась ли игра
     score_user_comp[0] = score_user_comp[0] + 1
     return score_user_comp
 
